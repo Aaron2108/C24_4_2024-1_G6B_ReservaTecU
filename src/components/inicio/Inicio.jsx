462e643676432bba609a42394629c6b06bbd0db1
Oliver Mendez <mendezoliver2108@gmail.com>
2024-06-24 22:30:33 -0500

    campos en pausa

--- src/components/inicio/Inicio.jsx
+++ src/components/inicio/Inicio.jsx
@@ -1,9 +1,17 @@
 import Header from "../NavBar/Header"
-
+import './inicio.css';
 const Inicio = () => {
   return (
-    <div>
+    <div className="div_inicio_Admin">
         <Header/>
+        <section className="section2_gestion_reservas">
+        <article className="article_gestion_reservas">
+            <img className="img_crear_reserva" src="/img/campo1.png" alt="" />
+            <section className="section_campos">
+                <h2>Campo 1</h2>
+            </section>
+        </article>
+      </section>
     </div>
   )
 }
